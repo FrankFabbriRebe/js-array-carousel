f17d4d0eb9eddea5dd0f895a58e3eb4af5a59ccc
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-23 14:50:50 +0100

    add: button

--- myscript.js
+++ myscript.js
@@ -3,3 +3,10 @@ const items = document.getElementsByClassName("item");
 //console.log(items);
 
 let activeItem = 0;
+
+// selection of the buttons
+const up = document.querySelector(".up");
+//console.log(up);
+
+const down = document.querySelector(".down");
+//console.log(down);
